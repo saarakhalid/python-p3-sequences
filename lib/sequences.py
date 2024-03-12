98b5ae05a3da11fcd2cb93dffcf2f600ddb7eec3
SaaraKhalid <khalidsaara6@gmail.com>
2024-03-12 13:54:12 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+    i, j = 0, 1
+
+    for _ in range(length):
+        my_list.append(i)
+        i, j = j, i + j
+
+    print(my_list) 
+
+
+print_fibonacci(0) 
+print_fibonacci(1) 
